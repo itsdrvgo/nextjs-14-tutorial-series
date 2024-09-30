def97812c3c626d8229b533729d86636bb31542c
itsdrvgo <thedragoluca@gmail.com>
2024-09-30 14:55:09 +0530

    ADD: ep10 Stylizing Your App with ShadCN UI

--- src/components/client.tsx
+++ src/components/client.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 export function ClientComponent() {
     const [count, setCount] = useState(0);
@@ -8,7 +9,7 @@ export function ClientComponent() {
     return (
         <div>
             <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <Button onClick={() => setCount(count + 1)}>Increment</Button>
         </div>
     );
 }
